5687d7a3eaa1e501406d9d1947c2e8dcb8fc1745
marcourakawa <marco.urakawa@gmail.com>
2024-12-03 22:02:50 -0300

    meu quinto commit

--- my_first_script.py
+++ my_first_script.py
@@ -1,9 +1,21 @@
-list = [1, 2, 3, 5]
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import WebDriverException
 
-texto = "Apenas testando o Git"
+# Definir o caminho para o ChromeDriver
+driver_path = r'C:\webdrivers\chromedriver.exe'
+service = Service(driver_path)
 
-numero = 1
-
-print(list)
-print(texto)
-print(numero)
+# Inicializar o WebDriver
+try:
+    driver = webdriver.Chrome(service=service)
+    driver.get('https://www.petz.com.br/nossas-lojas')
+    
+    # Verifica se a página foi carregada
+    print("Título da Página:", driver.title)
+    print("O site está acessível!")
+except WebDriverException as e:
+    print("Erro ao acessar o site:", e)
+finally:
+    # Fechar o navegador
+    driver.quit()
